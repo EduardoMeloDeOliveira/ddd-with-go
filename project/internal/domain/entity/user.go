2f841ce0b85a3de411a42b26805a4cd97632aebb
EduardoMeloDeOliveira <eduardo.moliveira.dev@hotmail.com>
2025-07-22 01:22:22 -0300

    feature: create project go with ddd pattern

---
+++ project/internal/domain/entity/user.go
@@ -0,0 +1,17 @@
+package entity
+
+import "github.com/google/uuid"
+
+type User struct {
+	ID    uuid.UUID `gorm:"type:uuid;primary_key"`
+	Name  string    `gorm:"size:500;not null"`
+	Email string    `gorm:"size:500;not null;uniqueIndex"`
+}
+
+func newUser(name string, email string) *User {
+	return &User{
+		ID:    uuid.New(),
+		Name:  name,
+		Email: email,
+	}
+}
