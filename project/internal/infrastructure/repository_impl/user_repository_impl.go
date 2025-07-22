790dbef0e308c69e095107d103fbcbf1fb0274d6
EduardoMeloDeOliveira <eduardo.moliveira.dev@hotmail.com>
2025-07-22 02:00:51 -0300

    feature: developing the controller

---
+++ project/internal/infrastructure/repository_impl/user_repository_impl.go
@@ -0,0 +1,42 @@
+package repository_impl
+
+import (
+	"github.com/EduardoMeloDeOliveira/ddd-with-go/project/internal/domain/entity"
+	"gorm.io/gorm"
+)
+
+type UserRepositoryImpl struct {
+	db *gorm.DB
+}
+
+func NewUserRepositoryImpl(db *gorm.DB) *UserRepositoryImpl {
+	return &UserRepositoryImpl{db: db}
+}
+
+func (repoImpl *UserRepositoryImpl) Save(user *entity.User) error {
+	return repoImpl.db.Save(user).Error
+}
+
+func (repoImpl *UserRepositoryImpl) FindById(id string) (*entity.User, error) {
+	var user entity.User
+
+	err := repoImpl.db.First(&user, "id = ?", id).Error
+
+	if err != nil {
+		return nil, err
+	}
+
+	return &user, nil
+}
+
+func (repoImpl *UserRepositoryImpl) FindAll() ([]*entity.User, error) {
+	var users []*entity.User
+
+	err := repoImpl.db.Find(&users).Error
+
+	if err != nil {
+		return nil, err
+	}
+
+	return users, nil
+}
